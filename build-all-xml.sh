d657a78d1cf075ba78b754adc415f4ee0cdad6ed
柳东原 · Dongyuan Liu <liu.dongyuan@gmail.com>
2019-11-19 20:54:15 +0800

    Build XML for all 4 dictionaries

---
+++ build-all-xml.sh
@@ -0,0 +1,4 @@
+./generate.py raw/ChineseUyghurStarDict_CnUy_ULY.txt chinese-uyghur-uly/MyDictionary.xml
+./generate.py raw/ChineseUyghurStarDict.txt chinese-uyghur-uey/MyDictionary.xml
+./generate.py raw/ChineseUyghurStarDict_UyCn_ULY.txt uyghur-chinese-uly/MyDictionary.xml
+./generate.py raw/ChineseUyghurStarDict_UyCn.txt uyghur-chinese-uey/MyDictionary.xml
